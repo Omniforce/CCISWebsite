d2c8cd0779485d6ce7d6a247b77c5af6a34ad0d5
Omniforce <brendanros@gmail.com>
2014-01-21 18:17:13 -0500

    Added a Color Converter
    Created and added a color converter which can be accessed by opening the
    new color converter button at the top of page.

--- content-switcher.js
+++ content-switcher.js
@@ -1,5 +1,6 @@
 var homeButton = document.getElementById("homeButton");
 var fwotdButton = document.getElementById("fwotdButton");
+var colorButton = document.getElementById("colorButton");
 var resumeButton = document.getElementById("resumeButton");
 var linksButton = document.getElementById("linksButton");
 var contactButton = document.getElementById("contactButton");
@@ -32,6 +33,10 @@ function showFwotd() {
     fwotdPage.style.display = "block";
 }
 
+function showColor() {
+    window.open('colorconverter/color-converter.html')
+}
+
 function showResume() {
 	window.open('pdfs/resume.pdf');
 }
@@ -59,13 +64,24 @@ function showFwotdInfo() {
 }
 
 function showFwotdResults() {
-	fwotdInfo.style.display = "none";
-	$('#fwotdResults').fadeIn('fast');
-	fwotdResults.style.display = "block";
+    if (document.getElementById("#summonerName") == null) {
+        document.getElementById("#Warning").display = "block";
+    } else {
+	   fwotdInfo.style.display = "none";
+	   $('#fwotdResults').fadeIn('fast');
+	   fwotdResults.style.display = "block";
+    }
 }
+
+// function showFwotdResults() {
+//     fwotdInfo.style.display = "none";
+//     $('#fwotdResults').fadeIn('fast');
+//     fwotdResults.style.display = "block";
+// }
 
 homeButton.onclick = showHome;
 fwotdButton.onclick = showFwotd;
+colorButton.onclick = showColor;
 resumeButton.onclick = showResume;
 linksButton.onclick = showLinks;
 contactButton.onclick = showContact;
