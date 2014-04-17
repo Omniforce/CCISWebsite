c70c3227023772c3d8dc6d2eae2aa9fdf265dfe4
Omniforce <brendanros@gmail.com>
2014-04-16 23:33:36 -0400

    Removed First Win of the Day page

--- content-switcher.js
+++ content-switcher.js
@@ -1,5 +1,4 @@
 var homeButton = document.getElementById("homeButton");
-var fwotdButton = document.getElementById("fwotdButton");
 var colorButton = document.getElementById("colorButton");
 var resumeButton = document.getElementById("resumeButton");
 var linksButton = document.getElementById("linksButton");
@@ -8,29 +7,14 @@ var submitButton = document.getElementById("submitButton");
 var backButton = document.getElementById("backButton");
 
 var homePage = document.getElementById("homePage");
-var fwotdPage = document.getElementById("fwotdPage");
 var linksPage = document.getElementById("linksPage");
 var contactPage = document.getElementById("contactPage");
 
-var fwotdInfo = document.getElementById("fwotdInfo");
-var fwotdResults = document.getElementById("fwotdResults");
-
 function showHome() {
-    fwotdPage.style.display = "none";
     linksPage.style.display = "none";
     contactPage.style.display = "none";
 	$('#homePage').fadeIn('slow');
     homePage.style.display = "block";
-}
-
-function showFwotd() {
-    homePage.style.display = "none";
-    linksPage.style.display = "none";
-    contactPage.style.display = "none";
-	fwotdResults.style.display = "none";
-	$('#fwotdPage').fadeIn('slow');
-	fwotdInfo.style.display = "block";
-    fwotdPage.style.display = "block";
 }
 
 function showColor() {
@@ -42,7 +26,6 @@ function showResume() {
 }
 
 function showLinks() {
-    fwotdPage.style.display = "none";
     homePage.style.display = "none";
     contactPage.style.display = "none";
 	$('#linksPage').fadeIn('slow');
@@ -50,40 +33,14 @@ function showLinks() {
 }
 
 function showContact() {
-    fwotdPage.style.display = "none";
     linksPage.style.display = "none";
     homePage.style.display = "none";
 	$('#contactPage').fadeIn('slow');
     contactPage.style.display = "block";
 }
 
-function showFwotdInfo() {
-	fwotdResults.style.display = "none";
-	$('#fwotdInfo').fadeIn('fast');
-	fwotdInfo.style.display = "block";
-}
-
-function showFwotdResults() {
-    if (document.getElementById("#summonerName") == null) {
-        document.getElementById("#Warning").display = "block";
-    } else {
-	   fwotdInfo.style.display = "none";
-	   $('#fwotdResults').fadeIn('fast');
-	   fwotdResults.style.display = "block";
-    }
-}
-
-// function showFwotdResults() {
-//     fwotdInfo.style.display = "none";
-//     $('#fwotdResults').fadeIn('fast');
-//     fwotdResults.style.display = "block";
-// }
-
 homeButton.onclick = showHome;
-fwotdButton.onclick = showFwotd;
 colorButton.onclick = showColor;
 resumeButton.onclick = showResume;
 linksButton.onclick = showLinks;
 contactButton.onclick = showContact;
-submitButton.onclick = showFwotdResults;
-backButton.onclick = showFwotdInfo;
